a23de74b0077f82227febc6ee946449bd7da40e9
Alga <algavania454@gmail.com>
2020-11-15 18:18:07 +0700

    update

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Upgrading</title>
+    <link rel="icon" href="images/logo.png" type="image/png">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <title>Reve Store</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
-            <img src="images/logo.png" width="40" height="30" class="d-inline-block align-top" alt="Logo">
+            <img src="images/logo.png" width="50" height="50" class="d-inline-block align-top" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -27,18 +33,18 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Members
+                        Products
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Irene</a>
-                        <a class="dropdown-item" href="#">Seulgi</a>
+                        <a class="dropdown-item" href="#">Clothes</a>
+                        <a class="dropdown-item" href="#">Shoes</a>
                     </div>
                 </li>
             </ul>
         </div>
     </nav>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -46,13 +52,25 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="images/bg.jpg" alt="First slide">
+                <img class="d-block w-100" src="images/bg-1.jpg" alt="Stylish slide" style="min-height: 760px;">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 style="font-size: 5rem;">Stylish</h1>
+                    <p class="h2">Style your everyday.</p>
+                </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="images/bg-2.jpg" alt="Second slide">
+                <img class="d-block w-100" src="images/bg-2.jpg" alt="Unique slide" style="min-height: 760px;">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 style="font-size: 5rem;">Unique</h1>
+                    <p class="h2">Suit your unique taste.</p>
+                </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="images/bg-3.jpg" alt="Third slide">
+                <img class="d-block w-100" src="images/bg-3.jpg" alt="Trendy slide" style="height: 760px;">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 style="font-size: 5rem;">Trendy</h1>
+                    <p class="h2">Confuse your mirror by our trendy outfits.</p>
+                </div>
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -65,4 +83,5 @@
         </a>
     </div>
 </body>
+
 </html>
